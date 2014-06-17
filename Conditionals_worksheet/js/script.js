3fb4cc42d86d1bd22e5bdc297f9ffa2206070c0f
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-17 16:35:23 -0400

    Need Gas?
    Working out formula, early stages.  Changes to phrasing and math issues.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,4 +4,11 @@ Conditionals_worksheet
 6/17/2014
  */
 
-alert("testing");
+//alert("testing"); Test was successful//
+
+var mpg = galUsed / milesTraveled;
+var galAvail = C
+var galUsed = C - galAvail;
+var capacity = C;
+var milesTraveled = X
+
