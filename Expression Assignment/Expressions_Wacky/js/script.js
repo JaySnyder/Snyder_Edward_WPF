2f4568e01f75e5789194823130a8e35496d5c2e1
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-16 23:38:21 -0400

    Final changes on wacky
    worked out bugs, transferred info properly.

--- Expression Assignment/Expressions_Wacky/js/script.js
+++ Expression Assignment/Expressions_Wacky/js/script.js
@@ -23,18 +23,20 @@ Expressions_Wacky
    var splits = prompt("How many people are you splitting this between?");//number of people contributing
 
 
-   var remainingCost = costOfkeg - whatsLeft;
+   var remainder = whatsLeft / splits;
 
 
-   var costPerperson = remainingCost / splits;
 
 
-   alert ("Your part of the cost" + costPerperson );//tells each person how much they'll have to pay
 
-   var howManycups = 1984 / splitBetween  ;//1984 are the oz's in a full keg / the number of people (split between)
+   alert ("Your part of the cost" + remainder );//tells each person how much they'll have to pay
+
+
+   var howManycups = 1984 / splits; //1984 are the oz's in a full keg / the number of people (split between)
 
 
    alert ("You can have this many 12oz cups: " + howManycups / 12 );//number of cups / 12 (ozs per cup)
+
 
 
 
