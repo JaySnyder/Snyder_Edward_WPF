08dca3fd04cdac43fc6db4a3f3f4e9d978a0ba54
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-18 19:05:32 -0400

    Done with nr.1
    Changes/challenges & now correct!!!!

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -10,30 +10,25 @@ Conditionals_worksheet
 
     var milesDriven = prompt ("How many miles have you driven? ");
 
-    //var gasUsed = prompt ("Percentage of gas used? "); original statement
-
-
-
-    var galUsed = prompt ("pct of gas used? ");//new statement
-
-    //var galUsed = gasUsed - tankCap;//
-
-    var mpg = milesDriven / galUsed;
-
-    alert("You're getting this many mpg: " + mpg);
+    var galUsed = prompt ("How many gallons of gas have you used? ");
 
     var galLeft = tankCap - galUsed;
 
+    var mpg = milesDriven / galUsed ;
+
+
+    alert("You're getting this many mpg: " + mpg);
+
     alert("You have this many gallons left:  " + galLeft);
 
-    var milesLeft =  mpg / galLeft;
+    var milesLeft = galLeft * mpg;
 
-    alert ("Miles you can drive:  " + milesLeft);
+   alert ("Miles you can drive:  " + milesLeft);
 
 
-   //if(milesLeft > 200){
-      //console.log("You can make the next gas station!");
-       //}else
-      // {
-      // console.log("You better stop for gas!");
-      // }
+     if(milesLeft > 200){
+      alert("You can make the next gas station!");
+       }
+        else{
+            alert("You need to stop for gas!")
+        }
