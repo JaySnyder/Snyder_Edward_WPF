f4c20e025cadcfc97cf5863faa67772fa3ad7912
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-17 20:55:08 -0400

    Changes continue to problem 1
    working out strings and verbiage

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -6,9 +6,25 @@ Conditionals_worksheet
 
 //alert("testing"); Test was successful//
 
+   var tankCap = prompt ("How many gallons does your tank hold? ");
+
+   var milesTraveled = prompt ("How many miles have you traveled thus far? ");
+
+   var 
+
+   var mpg = galStarted / milesTraveled;
+
+   var galStarted =
+
+
+
+
+
+   var gasLeft = prompt ("What percentage of your gas is left");
+
    var mpg = galUsed / milesTraveled;
 
-   var galAvail = tankCapacity - galUsed;
+   var gasAvail = tankCapacity - galUsed;
 
    var tankCapacity = X;
 
