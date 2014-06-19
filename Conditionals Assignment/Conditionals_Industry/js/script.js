d4cdbfd471192b2141b159a2916d1f78448e22bd
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-19 16:53:44 -0400

    Cleaning up conditionals

--- Conditionals Assignment/Conditionals_Industry/js/script.js
+++ Conditionals Assignment/Conditionals_Industry/js/script.js
@@ -11,16 +11,16 @@ Conditionals_Industry
 
    var iMac = 3750; //In $
 
-   alert("Amount needed to purchase new i-Mac $" + iMac);
+   alert("Amount needed to purchase new i-Mac $" + iMac); //Cost of new iMac
 
-   var c = prompt ("Cost per hour for concepting: ");
+   var c = prompt ("Cost per hour for conceptualizing: ");
 
-//Validate prompt
-//if(condition to test){ code that will run if true}
+      //Validate prompt
+      //if(condition to test){ code that will run if true}
 
-if(c ===""){
+    if(c ===""){
     //Reprompt for a new answer
-    c = prompt ("Cost per hour for concepting. Please don't leave blank! ");
+    c = prompt ("Cost per hour for conceptualizing. Please don't leave blank! ");
 }
 
 //Test if they typed in numbers
@@ -28,27 +28,27 @@ if(c ===""){
 //console.log(isNaN("Monkey")); //true -Boolean
 //console.log(isNaN(7)); //false -Boolean
 
-if(isNaN(c)){
+    if(isNaN(c)){
 
     //Reprompt for only numbers
-    c = prompt ("Cost per hour for concepting. Please use only numbers! ");
+    c = prompt ("Cost per hour for conceptualizing. Please use only numbers! ");
 
 }
 
 
 
-   var co = prompt ("How many hours of concepting? ");
+   var co = prompt ("How many hours of conceptualizing? ");
 
 //Combine both validations
-if(co==="" || isNaN(co)){
+   if(co==="" || isNaN(co)){
 
-    co = prompt ("How many hours of concepting?\nPlease don't leave blank and only use numbers.");
+    co = prompt ("How many hours of conceptualizing?\nPlease don't leave blank and only use numbers.");
 }
 
 
    total1 = c * co;
 
-   alert("Amount made concepting: " + total1);
+   alert("Amount made conceptualizing: " + total1);
 
 
    var p = prompt ("Cost per hour for production: ");
@@ -61,13 +61,13 @@ if(co==="" || isNaN(co)){
 
    var j = total1 + total2;
 
-   alert ("Total for production and concepting" + j);
+   alert ("Total for production and conceptualizing" + j);
 
    var t = j + s;
 
    alert ("Including savings you have: " + t);
 
 
-      (iMac < t + s ||  iMac < j ) ? ("We can buy the iMac!") : ("Sorry, we need more money");
+   (iMac < t + s ||  iMac < j ) ? ("We can buy the iMac!") : ("Sorry, we need more money");
 
 
