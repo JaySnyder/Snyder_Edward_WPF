2fa93a6f85f566ee994d2d2214884538e705ce61
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-19 16:06:19 -0400

    Conditionals Industry Changes
    Help from Dan

--- Conditionals Assignment/Conditionals_Industry/js/script.js
+++ Conditionals Assignment/Conditionals_Industry/js/script.js
@@ -7,21 +7,49 @@ Conditionals_Industry
 
 //alert("testing");conducted and working
 
-   var s = 632;
+   var s = 632; //In $
 
-   var iMac = 3750;
+   var iMac = 3750; //In $
 
-   alert("Amount needed to purchase new i-Mac" + imac);
+   alert("Amount needed to purchase new i-Mac $" + iMac);
 
    var c = prompt ("Cost per hour for concepting: ");
 
+//Validate prompt
+//if(condition to test){ code that will run if true}
+
+if(c ===""){
+    //Reprompt for a new answer
+    c = prompt ("Cost per hour for concepting. Please don't leave blank! ");
+}
+
+//Test if they typed in numbers
+//isNaN() - is not a number
+//console.log(isNaN("Monkey")); //true -Boolean
+//console.log(isNaN(7)); //false -Boolean
+
+if(isNaN(c)){
+
+    //Reprompt for only numbers
+    c = prompt ("Cost per hour for concepting. Please use only numbers! ");
+
+}
+
+
+
    var co = prompt ("How many hours of concepting? ");
+
+//Combine both validations
+if(co==="" || isNaN(co)){
+
+    co = prompt ("How many hours of concepting?\nPlease don't leave blank and only use numbers.");
+}
+
 
    total1 = c * co;
 
    alert("Amount made concepting: " + total1);
 
-  //validate here - just a test
 
    var p = prompt ("Cost per hour for production: ");
 
@@ -40,6 +68,6 @@ Conditionals_Industry
    alert ("Including savings you have: " + t);
 
 
-      (imac < t + s ||  t ) ? ("We can buy the iMac!") : ("Sorry, we need more money");
+      (iMac < t + s ||  iMac < j ) ? ("We can buy the iMac!") : ("Sorry, we need more money");
 
 
