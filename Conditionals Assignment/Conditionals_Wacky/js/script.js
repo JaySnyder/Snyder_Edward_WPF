f124e05ec184b1e260c7173880850a4d2c4153cc
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-19 20:54:26 -0400

    Working out verbage/validations/NaN

--- Conditionals Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals Assignment/Conditionals_Wacky/js/script.js
@@ -1,7 +1,34 @@
 /*
-JaySnyder_WPF
-Test
-6/10/2014
+JaySnyder
+Conditionals_wacky
+6/19/2014
  */
 
-alert("testing");
+//alert("testing"); Test ran successfully!
+
+   water = 279;
+
+   var teams = prompt ("How many teams are involved? ");
+
+   var balloons = prompt ("How many balloons per team? ");
+
+if(balloons===""){
+
+    balloons = prompt ("How many balloons per team?. Please don't leave blank! ");
+
+}
+
+if(isNaN(balloons)) {
+
+    //Reprompt for  numbers
+    balloons = prompt("How many balloons per team?. Please use only numbers! ");
+
+}
+
+
+   var amount = number(teams) + number(balloons);
+
+
+
+
+
