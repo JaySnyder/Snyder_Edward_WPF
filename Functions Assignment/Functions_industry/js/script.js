0a680482102c32971cb285af29b1d318b98ffab2
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-24 10:00:02 -0400

    Working out language and math
    Trying to figure out the anonymous function structure.

--- Functions Assignment/Functions_industry/js/script.js
+++ Functions Assignment/Functions_industry/js/script.js
@@ -6,6 +6,8 @@ Functions_industry
 
 //alert("testing");//Test successful
 
+   var perPersonsqFt = 150;
+
    var currentSpace = prompt("What is your current square footage?: ");
 
   if(currentSpace==="") {
@@ -17,26 +19,50 @@ Functions_industry
     currentSpace = prompt("What is your current square footage?. Please use only numbers! ");
    }
 
-   var employees = prompt("How many employees do you currently have?: ");
+   var employees = prompt("How many employees do you currently have? ");
 
   if(employees==="") {
 
-    employees  = prompt("How many employees do you currently have??. Please do not leave blank.");
+    employees = prompt("How many employees do you currently have?. Please do not leave blank.");
    }
 
-   if(isNaN(employees)){
-    employees = prompt("How many employees do you currently have??. Please use only numbers! ");
-}
+  if(isNaN(employees)) {
+      employees = prompt("How many employees do you currently have?. Please use only numbers! ");
+   }
+
+
+   var newHires = prompt ("How many people are you looking to hire? ");
+
+  if(newHires==="") {
+
+    newHires = prompt("How many people are you looking to hire?. Please do not leave blank.");
+   }
+
+  if(isNaN(newHires)) {
+
+    newHires = prompt("How many people are you looking to hire?. Please use only numbers! ");
+
+   }
+
+
+ 
 
 
 
 
 
 
+  //var spaceNeeded = function (newHires, employees, perPersonsqFt){
+//
+  //                 var spaceNeeded = newHires + employees * perPersonsqFt;
+//
+  //}
+   //var a = spaceNeeded(newHires,employees, perPersonsqFt);
+     //alert("You need this many square feet: " = a);
 
 
 
+//var spaceNeeded = Number(newHires) + Number(employees) * Number(perPersonsqFt);
 
+//alert("Space needed is; " + spaceNeeded);
 
-
-var hires = prompt ("How many")
