13f2cad545850a5f7b02ed788aad4e543a4dbd0f
JaySnyder <jay.snyder@thinkmsa.com>
2014-06-26 16:18:41 -0400

    Beginning wacky script
    Beginning wacky script

--- Functions Assignment/Functions_wacky/js/script.js
+++ Functions Assignment/Functions_wacky/js/script.js
@@ -4,4 +4,25 @@ Test
 6/10/2014
  */
 
-alert("testing");
+//alert("testing"); Test ran successfully
+
+
+var apple = 100;
+
+var cherry = 100;
+
+var blueberry = 100;
+
+var peach = 100;
+
+var aTotal = ("How many people chose apple pie? ");
+
+var cTotal = ("How many people chose cherry pie? ");
+
+var bTotal = ("How many people chose blueberry pie? ");
+
+var pTotal = ("How many people chose peach pie? ");
+
+var participants = Number(apple) + Number(cherry) + Number(blueberry) + Number(peach);
+
+alert("")
